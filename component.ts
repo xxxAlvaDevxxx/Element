c93264bccc24f1ab89a6c83bc1df816e78cee47e
xxxAlvaDevxxx <dtamjob@gmail.com>
2023-07-02 21:55:46 -0400

    correccion de bug

--- component.ts
+++ component.ts
@@ -1,32 +1,40 @@
 class $ {
-  component: HTMLElement;
-  childrenArr: $[];
-  constructor(tag:keyof HTMLElementTagNameMap, attributes:object) {
-    this.create(tag);
-    this.setAttributes(attributes);
-  }
+  component: HTMLElement|null;
+  children: $[] = [];
   create(tag:keyof HTMLElementTagNameMap) {
     this.component = document.createElement(tag);
     return this;
   }
   father(father: $) {
-    father.children(this);
+    father.addChildren(this);
     return this;
   }
   fatherElement(father: HTMLElement) {
+    if (!this.component) throw Error("component is null")
     father.appendChild(this.component);
     return this;
   }
-  children(...children: $[]) {
+  addChildren(...children: $[]) {
     children.forEach((child) => {
-      this.component.appendChild(child.getComponent());
-      this.childrenArr.push(child);
+      if (!this.component) throw Error("component is null")
+      if (!child.component) throw Error("child is null")
+      this.component.appendChild(child.component);
+      this.children.push(child);
     });
     return this;
   }
+  addChild(child:$){
+    if (!this.component) throw Error("component is null") 
+    if (!child.component) throw Error("child is null")
+    this.component.appendChild(child.component)
+    this.children.push(child)
+    return this
+  }
   removeChildren(...children: $[]){
     children.forEach((child)=>{
-      this.component.removeChild(child.getComponent())
+      if (!this.component) throw Error("component is null")
+      if (!child.component) throw Error("child is null")
+      this.component.removeChild(child.component)
     })
   }
   createAndAppendFather(tag: keyof HTMLElementTagNameMap, father: $) {
@@ -35,37 +43,51 @@ class $ {
     return this;
   }
   setAttribute(name: string, value: string) {
+    if (!this.component) throw Error("component is null")
     this.component.setAttribute(name, value);
     return this;
   }
   setAttributes(attributes:object) {
-    if (typeof attributes != "object") return Error("objects only");
+    if (typeof attributes != "object") throw Error("objects only");
     let arr = Object.entries(attributes);
     arr.forEach((arg) => {
+      if (!this.component) throw Error("component is null")
+
       this.component.setAttribute(arg[0], arg[1]);
     });
     return this;
   }
   setClassNames(...classNames: string[]) {
+    if (!this.component) throw Error("component is null")
     this.component.classList.add(...classNames);
     return this;
   }
-  find(selector) {
+  find(selector:keyof HTMLElementTagNameMap) {
     this.component = document.querySelector(selector)
     return this
   }
-  findChildren(selector) {
+  findChildren(selector:string) {
+    if (!this.component) throw Error("component is null")
     return this.component.querySelector(selector);
   }
-  getComponent() {
-    return this.component;
-  }
   text(text: string) {
+    if (!this.component) throw Error("component is null")
     this.component.appendChild(document.createTextNode(text));
     return this;
   }
   event(callback: (component: HTMLElement) => {}):$ {
+    if (!this.component) throw Error("component is null")
     callback(this.component);
     return this;
   }
 }
+
+export class $2 extends ${
+  constructor(create:keyof HTMLElementTagNameMap,attributes:object){
+    super()
+    this.create(create)
+    this.setAttributes(attributes)
+  }
+}
+
+export default $;
