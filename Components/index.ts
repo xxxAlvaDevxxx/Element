7ed1d6f3cb0766859dbc9632e565221160b4d951
xxxAlvaDevxxx <dtamjob@google.com>
2024-05-17 07:50:27 -0400

    feat():  component created

--- Components/index.ts
+++ Components/index.ts
@@ -238,3 +238,9 @@ export class $labelAndSelect extends $ {
 export function $Divider() {
   return new $("span", { class: "divider" });
 }
+
+export class $article extends $ {
+  constructor(attributes: object = {}) {
+    super("article", attributes);
+  }
+}
