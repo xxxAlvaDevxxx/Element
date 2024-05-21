fe597c53ffdb5c3af2428e9a90213ee85e453b38
xxxAlvaDevxxx <dtamjob@google.com>
2024-05-21 02:37:11 -0400

    fix(datalist methods): methods return class

--- Components/index.ts
+++ Components/index.ts
@@ -65,11 +65,13 @@ export class $datalist extends $ {
   }
   addOption(value: string) {
     this.addChild(new $option(value, { value: value }));
+    return this
   }
   setOptions(...values: Array<string>) {
     values.forEach((value) => {
       this.addOption(value);
     });
+    return this
   }
 }
 
