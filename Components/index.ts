cd841cdc0f792482dc9ba83d2072b86fd5c37da0
xxxAlvaDevxxx <dtamjob@google.com>
2024-06-04 23:12:12 -0400

    feat(windowBuffer, windowList, buffer): window manager

--- Components/index.ts
+++ Components/index.ts
@@ -1,4 +1,4 @@
-import { $ } from "..";
+import { $, $2 } from "..";
 import { $button } from "./button";
 import { $h1, $h2, $h3, $h4, $h5, $h6 } from "./h";
 import $input from "./input";
@@ -65,37 +65,37 @@ export class $datalist extends $ {
   }
   addOption(value: string) {
     this.addChild(new $option(value, { value: value }));
-    return this
+    return this;
   }
   setOptions(...values: Array<string>) {
     values.forEach((value) => {
       this.addOption(value);
     });
-    return this
+    return this;
   }
 }
 
 export class $nav extends $ {
-  constructor(attributes: object = {}){
-    super('nav', attributes);
+  constructor(attributes: object = {}) {
+    super("nav", attributes);
   }
 }
 
 export class $ul extends $ {
-  constructor(attributes: object = {}){
-    super('ul', attributes);
+  constructor(attributes: object = {}) {
+    super("ul", attributes);
   }
 }
 
 export class $li extends $ {
-  constructor(attributes: object = {}){
-    super('li', attributes);
+  constructor(attributes: object = {}) {
+    super("li", attributes);
   }
 }
 
 export class $ol extends $ {
-  constructor(attributes: object = {}){
-    super('ol', attributes);
+  constructor(attributes: object = {}) {
+    super("ol", attributes);
   }
 }
 
@@ -121,5 +121,72 @@ export class $footer extends $ {
 export class $main extends $ {
   constructor(attributes: object = {}) {
     super("main", attributes);
+  }
+}
+
+export class $windowBuffer extends $article {
+  $btnBuffer: $button;
+  constructor(nameBuffer: string, window: $2) {
+    super();
+    this.$btnBuffer = new $button(nameBuffer, (ctx) => {
+      // rise to buffer
+      let $buffer = ctx.father.father.father as $buffer;
+      // take to viewer
+      let $viewer = $buffer.viewer;
+      // confirm if there is a displayed window
+      // if so, set false status and then remove all children of the viewer
+      if ($viewer.children[0]) $viewer.children[0].status = false;
+      $viewer.removeAllChildren();
+      // display the window that was selected
+      $viewer.addChild(window);
+      this.status = true;
+    }).setStyle({ width: "100%" /* , padding: "0 1em"  */ });
+    this.setStyle({ display: "grid" });
+    this.addChild(this.$btnBuffer);
+  }
+}
+
+export class $windowList extends $section {
+  // button to create a new button to access the window
+  constructor(...windowsBuffer: Array<$windowBuffer>) {
+    super({ id: "windowListBuffer" });
+    this.setStyle({
+      /* backgroundColor: "#fff7", */
+      display: "flex",
+      position: "fixed",
+      bottom: "0",
+      height: "5%",
+      width: "100%",
+      gap: "0.15em",
+    });
+    windowsBuffer.forEach((_window) => this.addChild(_window));
+  }
+}
+
+export class $buffer extends $article {
+  $windowList: $windowList;
+  /* The buffer will be composed of: 
+    - container that is the buffer class itself 
+    - list of windows 
+    - a button to add a new window 
+    - selected window display 
+  */
+  // this is the part that contains the window that is displayed
+  viewer = new $section({ id: "viewerBuffer" }).setStyle({
+    display: "flex",
+    justifyContent: "center",
+  });
+  constructor(windowList: $windowList) {
+    // create the container of the entire buffer as such
+    super({ id: "buffer" });
+    this.$windowList = windowList;
+    this.setStyle({
+      width: "100%",
+      height: "100vh",
+      display: "grid",
+      alignContent: "center",
+    });
+    // this is the part that contains the window that is displayed
+    this.addChildren(this.viewer, this.$windowList);
   }
 }
