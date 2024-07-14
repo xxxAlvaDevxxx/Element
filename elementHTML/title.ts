18bf8b25a713e1ec7259c8df777e4b5130b7bceb
xxxAlvaDevxxx <dtamjob@google.com>
2024-07-14 05:10:12 -0400

    feat(elementHTML title): default parameter(text)

--- elementHTML/title.ts
+++ elementHTML/title.ts
@@ -1,7 +1,8 @@
 import { $ } from "..";
 
 export default class $title extends $ {
-  constructor(attributes: object = {}) {
+  constructor(text: string, attributes: object = {}) {
     super("title", attributes);
+    this.setText(text)
   }
 }
