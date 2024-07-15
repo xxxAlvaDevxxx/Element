883550fff35d1f2624d679b113f1ab121b4122c3
xxxAlvaDevxxx <dtamjob@google.com>
2024-07-15 19:34:14 -0400

    update(elementHTML a): element Type HTMLAnchorElement

--- elementHTML/a.ts
+++ elementHTML/a.ts
@@ -1,7 +1,8 @@
 import { $TagText, Target } from "../types";
 
 export default class $a extends $TagText {
-  constructor(text: string, href: string = "", alt: string="", target: Target, attributes: object = {}) {
+  element: HTMLAnchorElement;
+  constructor(text: string, href: string = "", alt: string="", target: Target = "", attributes: object = {}) {
     super("a", attributes, text);
     if (href) this.setAttribute({name:'href', value: href})
     if (alt) this.setAttribute({name:'alt', value: alt})
