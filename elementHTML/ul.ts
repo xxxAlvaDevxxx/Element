2a49abafc6baca2b83818fc2cabbdc5f6500064b
xxxAlvaDevxxx <dtamjob@google.com>
2024-07-16 23:18:56 -0400

    feat(add child to list): takes the text and transforms it to elementHTML li and adds to the list.

--- elementHTML/ul.ts
+++ elementHTML/ul.ts
@@ -1,7 +1,13 @@
-import { $ } from "..";
+import { $, $2 } from "..";
+import $li from "./li";
 
 export default class $ul extends $ {
   constructor(attributes: object = {}) {
     super("ul", attributes);
+  }
+  addChilToList(text: string) {
+    const child = new $li().setText(text);
+    this.addChild(child);
+    return this;
   }
 }
